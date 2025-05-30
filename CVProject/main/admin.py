1b8f82cc5b670fb76679e8b430d1a4fa8060bb5b
Andrij Kasych <andrijkasych@gmail.com>
2025-05-30 18:37:50 +0300

    Task 1: Created CVDoc model

--- CVProject/main/admin.py
+++ CVProject/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CVDoc, Project, Skill
+
+
+class ProjectInline(admin.StackedInline):
+    model = Project
+
+
+class SkillInline(admin.StackedInline):
+    model = Skill
+
+
+admin.site.register(Project)
+admin.site.register(Skill)
+
+
+@admin.register(CVDoc)
+class ComputerAdmin(admin.ModelAdmin):
+    inlines = (ProjectInline, SkillInline, )
+
--- CVProject/main/models.py
+++ CVProject/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class CVDoc(models.Model):
+    firstname = models.CharField(max_length=120)
+    lastname = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    bio = models.TextField(max_length=500, default='', blank=True)
+
+    def __str__(self):
+        return f"CV of {self.firstname} {self.lastname}"
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=400)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    cvDoc = models.ForeignKey(CVDoc, on_delete=models.CASCADE, related_name='projects')
+
+    def __str__(self):
+        return f"Project {self.title}"
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=30)
+    experience = models.IntegerField()
+    cvDoc = models.ForeignKey(CVDoc, on_delete=models.CASCADE, related_name='skills')
+
+    def __str__(self):
+        return f"{self.title}: {self.experience} years"
