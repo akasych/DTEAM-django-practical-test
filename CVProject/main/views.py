6df8fbbcb0878a280d8d44c2326f8a6b14cbdecc
Andrij Kasych <andrijkasych@gmail.com>
2025-06-02 09:44:44 +0300

    Task 1: Implemented List Page View

--- CVProject/main/views.py
+++ CVProject/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import CVDoc
 
-# Create your views here.
+
+def home_page(request):
+    all_cvs: [str] = list_cv_names()
+    return render(request, template_name="index.html", context={"all_cvs": all_cvs})
+
+
+def list_cv_names() -> [str]:
+    return [cv.get_full_name() for cv in CVDoc.objects.all()]
