23fde0724238f6402745eb842fdf5918d86561d2
Andrij Kasych <andrijkasych@gmail.com>
2025-06-10 10:17:35 +0300

    Task 5: Added DB docker container and django application switched to PostgreSQL

--- CVProject/CVProject/settings.py
+++ CVProject/CVProject/settings.py
@@ -77,9 +77,19 @@ WSGI_APPLICATION = 'CVProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cvdb',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
+
+    #
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
