9d528774a0b567794df31928ab89752ec965dd1a
Andrij Kasych <andrijkasych@gmail.com>
2025-06-20 14:37:49 +0300

    Task 1: static urls fixed to dynamic

--- CVProject/main/urls.py
+++ CVProject/main/urls.py
@@ -3,9 +3,9 @@ from .views import home_page, log_page, settings_page, cv_page, cv_pdf
 
 
 urlpatterns = [
-    path('',  home_page),
-    path('logs/',  log_page),
-    path('settings/',  settings_page),
-    path('cv/<int:cv_id>/',  cv_page),
-    path('cv-pdf/<int:cv_id>/<str:file_name>',  cv_pdf),
+    path('',  home_page, name='home'),
+    path('logs/',  log_page, name='logs'),
+    path('settings/',  settings_page, name='settings'),
+    path('cv/<int:cv_id>/',  cv_page, name='cv'),
+    path('cv-pdf/<int:cv_id>/<str:file_name>', cv_pdf, name='cv-pdf'),
 ]
